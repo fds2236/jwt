e481b4c53c9074e125f2f95575b2dc8807ef58b7
eunjong <fds2236@naver.com>
2024-04-05 13:36:36 +0900

    swagger 기능 추가

---
+++ src/main/java/com/spring/backend/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.spring.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    OpenAPI openAPI() {
+		return new OpenAPI().components(new Components())
+				.info(new Info()
+						.title("jwt_security")
+						.description("backend config 모듈 연습")
+						.contact(new Contact()
+								.name("eunjong")
+								.email("fds2236@naver.com")));
+	}
+}
